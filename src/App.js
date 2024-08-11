2e5c3c4610894c7cd6ee86e108fa371a4c90c40c
hut <changchunstep@126.com>
2024-08-11 16:24:00 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useCallback, useState, useEffect} from "react";
+
+import Chart from 'chart.js/auto';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
+
+import { Line } from "react-chartjs-2";
+import DigitalClock from './DigitalClock.jsx';
 
 function App() {
+  const handle = useFullScreenHandle();
+
+  const [temp, setTemp] = useState('--');
+  const [humidity, setHumidity] = useState('--');
+
+  useEffect(() => {
+      const intervalId = setInterval(() => {
+          // read file or call a api
+      }, 5000);
+
+      return () => {
+          clearInterval(intervalId);
+      }
+  }, []);
+
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "First dataset",
+        data: [33, 53, 85, 41, 44, 65],
+        fill: false,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      },
+      {
+        label: "Second dataset",
+        data: [33, 25, 35, 51, 54, 76],
+        fill: false,
+        borderColor: "#742774"
+      }
+    ]
+  };
+
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
+      <div className='App-Content'>
+
+        <button onClick={handle.enter}>
+          全屏
+        </button>
+
+        <FullScreen handle={handle}>
+          <DigitalClock></DigitalClock>
+          <div className='Chart'>
+            <Line data={data} />
+          </div>
+
+          <div>
+            <p>温度: {temp}˚C          温度: {humidity}%</p>
+          </div>
+
+        </FullScreen>
+
+        {/* <DigitalClock></DigitalClock>
+        <div className='Chart'>
+          <Line data={data} />
+        </div> */}
+      </div>
     </div>
   );
 }
